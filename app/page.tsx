463b56670b88fa0921819d020504886b62ac589f
Nuno Marques <ositaka@gmail.com>
2025-11-07 14:20:07 +0000

    Fix button on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function HomePage() {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Button size="lg" asChild>
-                <Link href="/components">
+                <Link href="/components" className='inline-flex items-center'>
                   {t.home.hero.cta.primary}
                   <ArrowRight className="ms-2 h-4 w-4" />
                 </Link>
